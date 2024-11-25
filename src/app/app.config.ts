6f5f70f9d88b69be93f761d7fb0b9788de5acb33
Danniel Martinez <111100025+d4lion@users.noreply.github.com>
2024-11-24 22:54:24 -0500

    📦️ dependency: Add ngx-spinner for loading indicators and remove unused test file (#5)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { apiLoadingInterceptor } from './core/interceptors/api-loading.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes) , provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([apiLoadingInterceptor])),
+    importProvidersFrom(
+      NgxSpinnerModule.forRoot()
+    ),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 };
