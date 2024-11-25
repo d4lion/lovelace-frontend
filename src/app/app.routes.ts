6d80dc8e0dbddba697c853ccd459163b2df51e21
Danniel Martinez <111100025+d4lion@users.noreply.github.com>
2024-11-24 22:00:15 -0500

    🎉 feat: CreateUserService and integrate user creation (#4)
    * 🔧 config: Add path aliases for guards and services in tsconfig
    
    * 🔧 config: Add HTTP client provider and update guard import path
    
    * ✨ feat: Add CreateUserService and integrate user creation in profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { userSessionGuard } from './guards/user-session.guard';
+import { userSessionGuard } from '@guards/user-session.guard';
 
 export const routes: Routes = [
     {path: 'quiz', component: QuizComponent, canActivate: [userSessionGuard]},
---
+++ src/types/IUser.ts
@@ -0,0 +1,6 @@
+export interface ICreateUserData {
+    id: number | string;
+    name: string;
+    lastName: string | null;
+    email: string;
+}
