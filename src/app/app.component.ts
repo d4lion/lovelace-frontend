8f01c4a4d8aa4f920ce864b111baa8070d495045
d4lion <d4lion.dev@gmail.com>
2024-11-24 22:53:19 -0500

    📦️ dependency: Add ngx-spinner for loading indicators and remove unused test file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '@components/nav-bar/nav-bar.component';
 import { Router } from '@angular/router';
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
