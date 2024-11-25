c1e0a67c8e2d666e72c71d3571dd9c924fb87c01
d4lion <d4lion.dev@gmail.com>
2024-11-24 20:30:37 -0500

    ✨ add: implement user session guard for quiz route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { userSessionGuard } from './guards/user-session.guard';
 
 export const routes: Routes = [
-    {path: 'quiz', component: QuizComponent},
+    {path: 'quiz', component: QuizComponent, canActivate: [userSessionGuard]},
     {path: 'profile', component: ProfileComponent}
 ];
 
