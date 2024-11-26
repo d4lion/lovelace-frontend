45c3e4a7da555670702ff7c5029a54819f4291b5
d4lion <d4lion.dev@gmail.com>
2024-11-26 01:30:42 -0500

    🎉 feat: Update navigation bar with suggestion history link and translate sign out text; enhance suggestion display with improved layout and flight search functionality

--- src/app/pages/suggestion/suggestion.component.ts
+++ src/app/pages/suggestion/suggestion.component.ts
@@ -3,6 +3,46 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SuggestionsService } from '@services/data/suggestions.service';
 import { Suggestion } from '@type/IUser';
+  
+enum AirportCodes {
+  "Playa del Carmen" = "PCM",
+  "Rio de Janeiro" = "GIG",
+  "Bariloche" = "BRC",
+  "Cusco" = "CUZ",
+  "Banff" = "YYC",
+  "Nueva York" = "JFK",
+  "Miami" = "MIA",
+  "Toronto" = "YYZ",
+  "Tulum" = "CUN",
+  "Ushuaia" = "USH",
+  "Punta Cana" = "PUJ",
+  "San Juan" = "SJU",
+  "Chicago" = "ORD",
+  "Los Ángeles" = "LAX",
+  "Honolulu" = "HNL",
+  "Aspen" = "ASE",
+  "Ciudad de México" = "MEX",
+  "Santorini" = "Santorini",
+  "Lisboa" = "LIS",
+  "Interlaken" = "ZRH",
+  "Granada" = "GRX",
+  "Zermatt" = "ZRH",
+  "París" = "CDG",
+  "Viena" = "VIE",
+  "Berlín" = "TXL",
+  "Ibiza" = "IBZ",
+  "Reykjavik" = "KEF",
+  "Algarve" = "FAO",
+  "Londres" = "LHR",
+  "Niza" = "NCE",
+  "Chamonix" = "CMX",
+  "Roma" = "FCO",
+  "Malta" = "MLA",
+  "Cartagena" = "CTG",
+  "Barcelona" = "BCN"
+}
+
+
 
 @Component({
   selector: 'app-suggestion',
@@ -12,23 +52,37 @@ import { Suggestion } from '@type/IUser';
   styleUrl: './suggestion.component.css'
 })
 export class SuggestionComponent {
-  constructor(private router: Router, private suggestionService: SuggestionsService) { }
+  constructor(
+    private suggestionService: SuggestionsService
+  ) {}
 
-  suggestions: Suggestion[] = []
-  
+  suggestions: Suggestion[] = [];
+
+  searchFly(ciudad: string) {
+    // Sugerencia de vuelo basado en es scrapping de la página de booking.com
+    window.open(
+      `https://flights.booking.com/flights/MDE.CITY-${
+        AirportCodes[ciudad as keyof typeof AirportCodes]
+      }.AIRPORT/?type=ROUNDTRIP&adults=1&cabinClass=ECONOMY&children=&from=MDE.CITY&to=${
+        AirportCodes[ciudad as keyof typeof AirportCodes]
+      }.AIRPORT&fromCountry=CO&fromLocationName=Medell%C3%ADn&depart=2024-12-28&return=2025-01-04`
+    );
+  }
 
   ngOnInit(): void {
-    this.suggestionService.getLastSuggestion(1018234129, {
+
+    const userId = sessionStorage.getItem('id')!;
+
+    this.suggestionService.getLastSuggestion(userId, {
       callBackComplete: () => {
-        console.log('complete')
+        console.log('complete');
       },
       callBackError: (error) => {
-        console.log('error', error)
+        console.log('error', error);
       },
       callBackSuccess: (data) => {
         this.suggestions = [data];
-      }
-    })
+      },
+    });
   }
-
 }
